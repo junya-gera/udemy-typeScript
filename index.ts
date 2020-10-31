fe697e896ad5377a781986a6fcad2c38f44b9ad3
Junya WADA <junya-wada@msen.jp>
2020-10-31 13:09:54 +0900

    オブジェクトに型をつける

--- index.ts
+++ index.ts
@@ -9,3 +9,17 @@ let single: string = 'hello';
 
 // 型推論
 let Value: boolean;
+
+// オブジェクトの型推論
+const person = { 
+    name: 'Jack',
+    age: 21
+}
+// オブジェクトの型注釈
+const animal: {
+    name: string; // ここはセミコロン
+    age: number;
+} = {
+    name: 'okoge',
+    age: 3, 
+}
