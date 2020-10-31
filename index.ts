43c215c7da090e60e7be39458953a33348d854d4
Junya WADA <junya-wada@msen.jp>
2020-10-31 12:52:07 +0900

    型注釈と型推論

--- index.ts
+++ index.ts
@@ -1,2 +1,11 @@
-const hello: string = 'hello';
-console.log(hello);
+// 型の付け方
+let hasValue: boolean = true;
+// JS,TS は number は 浮動小数点数
+let count: number = 10;
+let single: string = 'hello';
+
+// マウスを変数の上に置くと型を確認できる
+// VScode の中に TSのコンパイラが内臓されている
+
+// 型推論
+let Value: boolean;
