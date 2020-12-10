64cd89d7ba90cce4e115943e4070b0676ba140bb
Junya WADA <junya-wada@msen.jp>
2020-12-10 23:27:58 +0900

    watch モードで自動でコンパイルする

---
+++ compiler/compiler.js
@@ -0,0 +1,3 @@
+// tsc compiler.ts --watch で watch モードになり、保存するたびに JS にコンパイルしてくれる
+var isActive = true;
+var likeCount = 10;
---
+++ compiler/compiler.ts
@@ -0,0 +1,4 @@
+// tsc compiler.ts --watch で watch モードになり、保存するたびに JS にコンパイルしてくれる
+
+let isActive = true;
+let likeCount = 10;
